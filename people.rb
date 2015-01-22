34759b562b94d6955dc701f1e8dddd3c3f30319f
Jenia Tretiyakova <zhanyat@gmail.com>
2015-01-22 14:11:06 -0500

    Created class Person that is shared among two other classes (Student and Instructor). Within a peroson class I created a method that is also shared by Student and Instructor classes. Then two classes were created Student and Instructor with the method inside of them that are specific to Student class and Instructor class only. Then called the method of student, instructor and used a common method which prompts for the name of student and instructor.

--- people.rb
+++ people.rb
@@ -1,7 +1,39 @@
-class Student
-
+#created class person that is shared 
+class Person 
+	def initialize(name)
+		@name = name 
 	end
 
-class Instructor
-
+	def greeting
+	"Hi my name is #{@name}"
 	end
+
+end
+
+
+class Student < Person
+def learn 
+	 "I get it!"
+	end
+end
+
+class Instructor < Person 
+def teach
+	 "Everythihg in Ruby is an Object"
+	
+end
+	end 
+
+
+student = Student.new("Christina")
+puts student.greeting
+
+student2 = Student.new("jenia")
+puts student2.greeting
+
+instructor = Instructor.new("Chris")
+puts instructor.greeting
+
+puts student.learn
+puts instructor.teach 
+
